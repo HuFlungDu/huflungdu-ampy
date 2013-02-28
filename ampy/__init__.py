fdd2c26b1e2cdb0046c6b8a070117b0530049be3
Josiah Baldwin <jbaldwin8889@gmail.com>
2013-02-28 10:54:11 -0600

    Initial commit

---
+++ ampy/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2006 Eric P. Mangold
+# See LICENSE for details.
+
+# Edits by <jbaldwin8889>
+# Imported ampy and async automatically
+# See SUBLICENSE for licensing details
+
+from ampy import *
+from async import *
